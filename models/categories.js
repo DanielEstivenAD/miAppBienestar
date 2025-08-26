65239716301926fab4545cdf87d4a32fe06ba0c0
DanielEstivenAD <danifra14once@gmail.com>
2025-08-26 07:55:11 -0500

    update package

--- models/categories.js
+++ models/categories.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Categories.hasMany(models.Events, {
-        foreignKey: 'CategoryId',
+        foreignKey: 'categoryId',
         as: 'events'
       });
     }
--- models/events.js
+++ models/events.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
         Events,this.belongsTo(models.Categories, {
-        through: "categories",
+        through: "Categories",
         as: "category"
       })
     }
